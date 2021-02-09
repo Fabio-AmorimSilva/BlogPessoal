e02a9d7727a6ceee52443c82e01967c7721f6e19
Fabio <fabioamorim2025@hotmail.com>
2021-02-09 18:33:09 -0300

    Adicionando Bootstrap as páginas

---
+++ firstProject/webpack.config.js
@@ -0,0 +1,26 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+
+module.exports = {
+
+	module:{
+        rules:[
+            {
+                test:/\.css$/,
+                use:['style-loader','css-loader']
+            }
+       ]
+    },
+
+
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: "Webpack Output",
+		}),
+	],
+
+
+};
+
+
+
